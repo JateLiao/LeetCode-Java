978a084fca8043a6bf7e4c0336c7dc504d80e3ac
Jate Liao <3746826117@qq.com>
2018-04-22 00:59:11 +0800

    leetcode

---
+++ src/com/lsj/leetcode/Info.java
@@ -0,0 +1,12 @@
+package com.lsj.leetcode;
+
+/**
+ * @Title: ${FILE_NAME}
+ * @Company: com.lsj
+ * @Package com.lsj.leetcode
+ * @Description: ${TODO}
+ * @Author liao
+ * @Createtine 2018/4/220:50
+ */
+public class Info {
+}

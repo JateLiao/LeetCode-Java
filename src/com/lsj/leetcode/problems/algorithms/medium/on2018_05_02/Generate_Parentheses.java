28fada1d10738045b686d2c51c34e23b1343c111
Jate Liao <3746826117@qq.com>
2018-05-02 23:35:07 +0800

    括号生成:generateParenthesis

---
+++ src/com/lsj/leetcode/problems/algorithms/medium/on2018_05_02/Generate_Parentheses.java
@@ -0,0 +1,41 @@
+package com.lsj.leetcode.problems.algorithms.medium.on2018_05_02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Title: ${FILE_NAME}
+ * @Company: com.lsj
+ * @Package com.lsj.leetcode.problems.algorithms.medium.on2018_05_02
+ * @Description: ${TODO}
+ * @Author liao
+ * @Createtine 2018/5/223:26
+ */
+public class Generate_Parentheses {
+    /**
+     * 括号生成
+     * 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+     * 例如，给出 n = 3，生成结果为：
+     * [
+     *   "((()))",
+     *   "(()())",
+     *   "(())()",
+     *   "()(())",
+     *   "()()()"
+     * ]
+     */
+    
+    /**
+     * main method.
+     **/
+    public static void main(String[] args) {
+        List<String> pathes = generateParenthesis (3);
+    }
+    
+    public static List<String> generateParenthesis(int n) {
+        List<String> parenthesis = new ArrayList<> ();
+        
+        
+        return parenthesis;
+    }
+}

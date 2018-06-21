f9beebc60eb99894ee2bcb630b2cc4a98b702f0c
Jet Liao <374682617@qq.com>
2018-06-21 21:38:00 +0800

    IsNumber

---
+++ src/com/lsj/leetcode/problems/algorithms/hard/on2018_06_21/IsNumber.java
@@ -0,0 +1,37 @@
+package com.lsj.leetcode.problems.algorithms.hard.on2018_06_21;
+
+/**
+ * @Desc IsNumber
+ * @ProjectName LeetCode-Java
+ * @Company com.lsj
+ * @CreateTime 2018/6/21 21:35
+ * @Author tianzhong
+ */
+public class IsNumber {
+    /**
+     * 验证给定的字符串是否为数字。
+     * 例如:
+     * "0" => true
+     * " 0.1 " => true
+     * "abc" => false
+     * "1 a" => false
+     * "2e10" => true
+     * 说明: 我们有意将问题陈述地比较模糊。在实现代码之前，你应当事先思考所有可能的情况。
+     */
+    /**
+     * Main Method: 请开始你的操作.
+     *
+     */
+    public static void main(String[] args) {
+        String string = "";
+        long s = System.currentTimeMillis ();
+        boolean values = new IsNumber ().isNumber (string);
+        System.err.println("IsNumber：" + values);
+        System.err.println("耗时：" + String.valueOf (System.currentTimeMillis () - s));
+    }
+    public boolean isNumber(String s) {
+        boolean res = false;
+        
+        return res;
+    }
+}

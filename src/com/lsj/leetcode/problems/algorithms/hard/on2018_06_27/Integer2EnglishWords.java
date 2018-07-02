7a0893c8db613e8a20a65f9e52db9a3e4ddc6847
Jet Liao <374682617@qq.com>
2018-07-02 16:33:13 +0800

    numberToWords：代码75%

--- src/com/lsj/leetcode/problems/algorithms/hard/on2018_06_27/Integer2EnglishWords.java
+++ src/com/lsj/leetcode/problems/algorithms/hard/on2018_06_27/Integer2EnglishWords.java
@@ -43,6 +43,7 @@ public class Integer2EnglishWords {
     
     private static final String[] NUM_ARR = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
     private static final String[] NUM_TENS_ARR = {"Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private static final String[] NUM_TENS_JR_ARR = {"Eleven", "Twelve", "Thirteen", "Forteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
     private static final String[] UNIT_ARR = {"Hundred", "Thousand", "Million", "Billion"};
 
     public String numberToWords(int num) {
@@ -54,10 +55,34 @@ public class Integer2EnglishWords {
         String[] numArr = new String[len % 3 == 0 ? len / 3 :  len / 3 + 1]; // 三位数字数组
         int lenArr = numArr.length;
         StringBuilder valueSb = new StringBuilder ();
+        String value = "";
     
         for (int i = 0; i < lenArr; i++) {
             int endIndex = len - i * 3;
-            numArr[lenArr - 1 - i] = numStr.substring (Math.max (endIndex - 3, 0), endIndex);
+            String numSub = numStr.substring (Math.max (endIndex - 3, 0), endIndex);
+            numArr[lenArr - 1 - i] = numSub;
+            
+            while (numSub.charAt (0) == '0') { // 首部0清除
+                numSub = numSub.substring (1, numSub.length ());
+            }
+            
+            int lenSub = numSub.length ();
+            char[] arr = numSub.toCharArray ();
+            for (int j = 0; j < arr[i]; j++) {
+                char c = arr[i];
+                if (c == '0') {
+                    continue;
+                }
+                valueSb.append (NUM_ARR[Integer.valueOf (String.valueOf (c))]);
+                if (lenSub == 3) {
+                    valueSb.append (UNIT_ARR[0]);
+                } else if (lenSub == 2 && c != '0') {
+                    valueSb.append (UNIT_ARR[0]);
+                } else if (lenSub == 1 && c != '0') {
+                    valueSb.append (NUM_ARR[Integer.valueOf (String.valueOf (c))]);
+                }
+                --lenSub;
+            }
         }
         
         return valueSb.toString ();
